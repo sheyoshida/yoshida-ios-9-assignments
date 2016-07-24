29e9b1016c8e4c1c928d705ddfc8c9ae1ea4dc84
shenayoshida <sheyoshida@gmail.com>
2016-07-24 16:17:38 -0400

    added textfields for add entry

--- assignment_three/assignment_three/AddItemViewController.swift
+++ assignment_three/assignment_three/AddItemViewController.swift
@@ -17,13 +17,17 @@ protocol AddViewControllerDelegate {
 
 class AddItemViewController: UIViewController {
     
+    @IBOutlet weak var textFieldItemName: UITextField!
+    @IBOutlet weak var textFieldItemDescription: UITextField!
+    @IBOutlet weak var textFieldItemCookTime: UITextField!
+    
     var delegate: AddViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     @IBAction func cancelButtonTapped(sender: UIBarButtonItem) {
         
         dismissViewControllerAnimated(true, completion: nil)
@@ -31,15 +35,31 @@ class AddItemViewController: UIViewController {
     
     @IBAction func saveButtonTapped(sender: UIBarButtonItem) {
         
+        guard
+            let name = textFieldItemName.text,
+            let description = textFieldItemDescription.text,
+            let time = Int(textFieldItemCookTime.text!)
+            else {
+                return
+        }
+        
+        // TODO: add way to populate individual entry items for ingredients, steps and add camera for image. 
+        
+        let newRecipe = Recipe(name: name, cookTime: time, description: description, ingredients: [], steps: [], image: UIImage(named: "placeholder")!)
+        
+        delegate?.userAddedRecipeItem(newRecipe)
+        
+        // MARK: create test item for delegate
+        
         // collect item for delegate to pass
-        let recipe = Recipe(name: "Hot Dog Sushi", cookTime: 5, description: "Uncle Stan's contribution to the Yoshida family recipe book.", ingredients: ["leftover white rice", "hot dogs"], steps: ["make ball with sticky rice", "cook and slice hot dog", "insert chunk of hot dog into rice ball", "feel like a single guy"], image: UIImage(named: "banana-split")!)
+        let recipe = Recipe(name: "Hot Dog Sushi", cookTime: 10, description: "Uncle Stan's contribution to the Yoshida family recipe book.", ingredients: ["leftover white sticky rice", "hot dogs", "soy sauce", "nori seaweed"], steps: ["make ball with sticky rice", "cook hot dog in a fry pan with soy sauce", "wrap hot dog with rice and nori", "slice into bite-sized rounds"], image: UIImage(named: "hot-dog-sushi")!)
         
         // access delegate method and pass in desired data
         delegate?.userAddedRecipeItem(recipe)
         
         // dismiss view
-         dismissViewControllerAnimated(true, completion: nil)
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
-
+    
 }
